2c9fe707eef71e3bf70f8062ab888b03533406bb
Eoin jimenez <90059007+Ej043465@users.noreply.github.com>
2021-09-07 16:56:55 +0000

    Initial commit

---
+++ Sketch.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+
+  public void settings() {
+    size(400, 400);
+  }
+
+  public void setup() {
+    background(222);
+     //Cluster
+    fill(127, 42, 60);
+    ellipse(50,70,30,50);
+    ellipse(90,50,30,50);
+    ellipse(130,50,30,50);
+    ellipse(170,70,30,50);
+    //Lower
+    ellipse(110,130,100,100);
+  }
+
+
+    
+  }
